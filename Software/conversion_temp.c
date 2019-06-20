9ae7853ff5d0773c430341d847043604d804d214
José Cancinos <jmcancinos97@gmail.com>
2019-06-20 23:55:06 +0000

    Upload New File

---
+++ Software/conversion_temp.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+int fahr_asm(int,int);
+
+int fahrenheit(float celsius)
+{
+	/*  FAHRENHEIT = (CELSIUS * 1.8) + 32
+	*	Aproximamos a:
+	* 	F = (C*2) + 32
+	*/
+	int valor = (int) celsius;
+	int mul = 2;
+	valor = fahr_asm(valor,mul);
+	
+	return valor;
+}
+
+int main (void)
+{
+	float celsius = 10;
+	printf( "Celsius: %.2f°C\n", celsius );
+	int fahr = fahrenheit(celsius);
+	printf( "Fahrenheit: %d °F\n", fahr );
+
+	return 0;
+
+}

7d87aac2b356edbab62b142fceb3131313167c62
Nahuel David Oliva <nadaol@hotmail.com>
2019-06-22 16:14:05 +0000

    Update pwm module para control rgb

--- Software/Modulo_Kernel_Pwm/pwmDriverPin18.c
+++ Software/Modulo_Kernel_Pwm/pwmDriverPin18.c
@@ -1,4 +1,3 @@
-#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/gpio.h>
 #include <linux/kernel.h>
@@ -15,13 +14,24 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Nadaol");
  
 /* GPIO*/
-#define pwm1 	18 // Kernel PWM channel 0 on GPIO18
- 
-unsigned int duty 		= 50;		// Dutycycle [%](Lectura por medio del fs sys/pwm/pwm)
-unsigned int Period 	= 2;		// Periodo [ms] (Lectura por medio del fs sys/pwm/pwm)
-unsigned int enable		= 1;		//Enable pwm ->1 Habilitado
-unsigned int tmsec_On	= 1;		//Tiempo On [ms]
-unsigned int tmsec_Off	= 1;		//Tiempo Off [ms]
+unsigned const int PinRed = 17;
+unsigned const int PinGreen = 27;
+unsigned const int PinBlue = 22;
+ /*Variables de control*/
+unsigned static int RedDuty=0;		// Dutycycle [%](Lectura por medio del fs sys/pwm/pwm)
+unsigned static int GreenDuty=0;	//valores iniciales
+unsigned static int BlueDuty=5;
+unsigned static int RedOn;
+unsigned static int GreenOn;
+unsigned static int BlueOn;
+unsigned static int RedOff;
+unsigned static int GreenOff;
+unsigned static int BlueOff;
+unsigned static int freq 		= 1;		// Frecuencia [Hz] (Lectura por medio del fs sys/pwm/pwm)
+unsigned static int enable		= 1;		//Enable pwm ->1 Habilitado
+static void calcPeriodos(void);
+
+
 /*----------------------------------------------*/
 
 
@@ -29,34 +39,82 @@ unsigned int tmsec_Off	= 1;		//Tiempo Off [ms]
 int pwm_gpio_init(void)
 {
 	printk(KERN_INFO "Started GPIO initialization in %s\n", __FUNCTION__);
-	gpio_request(pwm1, "pwm1");
-	gpio_direction_output(pwm1, 0);
-	
+	gpio_request(PinRed, "Red");
+	gpio_request(PinGreen, "Green");
+	gpio_request(PinBlue, "Blue");
+	gpio_direction_output(PinRed, 0);
+	gpio_direction_output(PinGreen, 0);
+	gpio_direction_output(PinBlue, 0);
 	return 0;
 }
  
 void pwm_gpio_exit(void)
 {
 	printk(KERN_INFO "Releasing GPIO ports in %s \n", __FUNCTION__);
-	gpio_free(pwm1);
+	gpio_free(PinRed);
+	gpio_free(PinGreen);
+	gpio_free(PinBlue);
 }
 /*----------------------------------------------*/
 
 /*Funciones para el pwm (ejecutada en un segundo thread)*/
-#define THREAD_NAME0 "pwmPin"	// Nombre del hilo
+#define THREAD_NAME0 "pwmRedPin"	// Nombre del hilo
 struct task_struct *task0;		//tarea
 /* Tarea del thread*/
-int pwm_threadRun(void *data) 
+int pwm_Run_Red(void *data) 
 {
 	while(1)  {
-		if(enable){/*Generacion del pwm*/
-			gpio_set_value(pwm1, 1);
-			msleep(tmsec_On);
-			gpio_set_value(pwm1, 0);
-			msleep(tmsec_Off);
+		if(enable && RedDuty!=0){/*Generacion del pwm*/
+			gpio_set_value(PinRed,0);
+			if(RedOn>10000)msleep(RedOn/1000);
+			else usleep_range(RedOn,RedOn);
+			gpio_set_value(PinRed,1);
+			if(RedOff>10000)msleep(RedOff/1000);
+			else usleep_range(RedOff,RedOff);
 		} else {
-			gpio_set_value(pwm1,0);
-			msleep(5000);
+			gpio_set_value(PinRed,1);
+			msleep(200);
+		}
+		if (kthread_should_stop()) break;//Si se produce un error,detengo el hilo
+	}
+	return 0;
+}
+#define THREAD_NAME1 "pwmGreenPin"	// Nombre del hilo
+struct task_struct *task1;		//tarea
+int pwm_Run_Green(void *data) 
+{
+	printk(KERN_INFO "Starting Green: Pin %d Duty %d On %d Off %d",PinGreen,GreenDuty,GreenOn,GreenOff);
+	while(1)  {
+		if(enable && GreenDuty!=0){/*Generacion del pwm*/
+			gpio_set_value(PinGreen, 0);
+			if(GreenOn>10000)msleep(GreenOn/1000);
+			else usleep_range(GreenOn,GreenOn);
+			gpio_set_value(PinGreen, 1);
+			if(GreenOff>10000)msleep(GreenOff/1000);
+			else usleep_range(GreenOff,GreenOff);
+		} else {
+			gpio_set_value(PinGreen,1);
+			msleep(200);
+		}
+		if (kthread_should_stop()) break;//Si se produce un error,detengo el hilo
+	}
+	return 0;
+}
+#define THREAD_NAME2 "pwmBluePin"	// Nombre del hilo
+struct task_struct *task2;		//tarea
+int pwm_Run_Blue(void *data) 
+{
+	while(1)  {
+		if(enable && BlueDuty!=0){/*Generacion del pwm*/
+			gpio_set_value(PinBlue, 0);
+			if(BlueOn>10000)msleep(BlueOn/1000);
+			else usleep_range(BlueOn,BlueOn);
+			gpio_set_value(PinBlue, 1);
+			if(BlueOff>10000)msleep(BlueOff/1000);
+			else usleep_range(BlueOff,BlueOff);
+		} else {
+			gpio_set_value(PinBlue,1);
+			msleep(200);
 		}
 		if (kthread_should_stop()) break;//Si se produce un error,detengo el hilo
 	}
@@ -65,8 +123,11 @@ int pwm_threadRun(void *data)
 /* funcion de inicializacion del hilo */
 int pwm_runThread_init(void)
 {
-	printk(KERN_ALERT "STARTING PWM: Period is %d milliseconds, and dutycycle is %d percent.\n", Period, duty);
-	task0 = kthread_run(pwm_threadRun, NULL, THREAD_NAME0);
+	calcPeriodos();
+	printk(KERN_ALERT "STARTING PWM");
+	task0 = kthread_run(pwm_Run_Red, NULL, THREAD_NAME0);
+	task1 = kthread_run(pwm_Run_Green, NULL, THREAD_NAME1);
+	task2 = kthread_run(pwm_Run_Blue, NULL, THREAD_NAME2);
 	return 0;
 }
 /* Función de finalizacion del hilo */
@@ -74,6 +135,8 @@ void pwm_runThread_exit(void)
 {
 	printk(KERN_ALERT "STOPPING PWM in %s.\n", __FUNCTION__);
 	kthread_stop(task0);
+	kthread_stop(task1);
+	kthread_stop(task2);
 }
 /*----------------------------------------------*/
 
@@ -82,14 +145,25 @@ static struct kobject *pwm_kobject;
  
 static ssize_t set_pwm(struct kobject *kobj, struct kobj_attribute *attr, const char *buff, size_t count)
 {
-	sscanf(buff, "%d %d %d", &enable, &Period, &duty);
-	printk(KERN_INFO "Read values: %d %d %d", enable, Period, duty);
-	/* Calculo tiempos para generar el pwm con el duty cycle leido del fs sys/pwm/pwm*/
-	if(enable)  {
-		tmsec_On = (Period*duty)/100;	// Duration of on-cycle...
-		tmsec_Off = Period-tmsec_On;	// Duration of off-cycle...
-	}
+	sscanf(buff, "%d %d %d %d %d", &enable, &freq, &RedDuty,&GreenDuty,&BlueDuty);
+	RedDuty=RedDuty*100/255;
+	GreenDuty=GreenDuty*100/255;
+	BlueDuty=BlueDuty*100/255;
+	calcPeriodos();
+	printk(KERN_INFO "Read values: %d %d %d %d %d", enable, freq, RedDuty,GreenDuty,BlueDuty);
+	/* Calculo tiempos para generar el pwm con el duty cycle y freq leido del fs sys/pwm/pwm*/
+
+		
 	return count;
+}
+static void calcPeriodos()
+{
+RedOn =((1000000/freq)*RedDuty)/100;// Duration of ON-Period
+GreenOn =((1000000/freq)*GreenDuty)/100;	
+BlueOn =((1000000/freq)*BlueDuty)/100;	
+RedOff = ((1000000/freq)*(100-RedDuty))/100;// Duration of OFF-Period
+GreenOff = ((1000000/freq)*(100-GreenDuty))/100;
+BlueOff = ((1000000/freq)*(100-BlueDuty))/100;
 }
  
 static struct kobj_attribute pwm_attribute = __ATTR(pwm, (S_IWUSR | S_IRUGO), NULL, set_pwm);

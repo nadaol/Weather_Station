57599e1d0c2a4235aa4f799e876d82ac59519919
José Cancinos <jmcancinos97@gmail.com>
2019-06-27 14:16:52 +0000

    Replace fahr_asm.s

--- Software/Sistema-Python-C-Assembler/fahr_asm.s
+++ Software/Sistema-Python-C-Assembler/fahr_asm.s
@@ -1,5 +1,6 @@
 @ fahr_asm.s
-@ Descripcion: suma la constante 32 para lograr temp Fahrenheit
+@ Descripcion: multiplica por 2 y suma la constante 32 
+@	       para lograr temp Fahrenheit
 
 @ Info de Raspberry
         .cpu    cortex-a53
@@ -9,29 +10,41 @@
 @ Datos
 	
 	.data
-	.align  2	
+	.align  4	
 cons:	.word	32		@defino variable cons valor 32 dec 
 
 @ Codigo
         .text
-        .align  2
+        .align  4
         .global fahr_asm
         .type   fahr_asm, %function
 	
 fahr_asm:
-        str     fp, [sp, -4]!   @ Salvo contexto 
-        add     fp, sp, 0       @ reubico frame pointer
+        str     fp, [sp, #-4]!  @ Salvo contexto - guardo direccion de retorno 
+        add     fp, sp, #0      @ nuevo frame pointer = stack pointer
 
-	mov	r2, #0		@ vacio el registro r2
-	add 	r2,r1		@ agrego el valor del 2do parametro mul
+	sub	sp, sp, #12	@ espacio para variables locales
+	str	r0, [fp, #-8]	@ guardo el primer parametro en r0 
+	str	r1, [fp, #-12]	@ guardo el segundo parametro en r1 
+	
+	ldr	r2, [fp, #-8]	@ cargo en r2 como una variable local
+				@ el valor del primer parametro
 
-	mul	r0,r2		@ multiplico a r0 el valor de r2
+	ldr 	r3, [fp, #-12]	@ cargo en r3 como una variable local
+				@ el valor del segundo parametro
 
-	ldr	r1, =cons	@ guardo en r1 la direccion de cons
-        ldr	r1,[r1]		@ guardo en r1 el valor almacenado en r1
-				@ r0 contiene el argumento pasado por C
-	add	r0,r1		@ sumo r1 a r0 y guardo en r0
+	mul	r3, r2, r3	@ multiplico r2 y r3 y lo guardo en r3
 
-        sub     sp, fp, 0	@ devuelvo valor a stack pointer 
-        ldr     fp, [sp], 4     @ devuelvo el valor de frame pointer de llamada
-        bx      lr              @ retorno
+	ldr	r2, =cons	@ guardo en r2 la direccion de cons
+        ldr	r2,[r2]		@ guardo en r2 el valor almacenado en r2
+				
+	add	r3, r2, r3	@ sumo r2 a r3 y guardo en r3
+
+	mov	r0, r3		@ el valor de r3 lo almaceno en r0
+
+	add	sp, fp, #0	@ retorno el valor original del frame pointer 
+				@ stack pointer = frame pointer
+
+        ldr     fp, [sp], #4    @ cargo direccion de retorno y quito parametros de la pila
+
+	bx      lr              @ retorno
